a01de1d63b915634418a8ed97ba9926ec00662f1
Max Fowler <max_fowler@brown.edu>
2013-08-03 13:30:12 -0400

    latest

--- truespeak/urls.py
+++ truespeak/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from truespeak.views import viewWrapper, login_page, home, redirect, register
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'truespeak.views.home', name='home'),
-    # url(r'^truespeak/', include('truespeak.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+   (r'^login/$', login_page),
+   (r'^register/$', register),
+   (r'^home/$', viewWrapper(home)),
+   (r'.*$',  redirect, {'page':"/home/"}),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
