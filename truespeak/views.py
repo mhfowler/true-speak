2d7e7a45a26048cd7bc8072954eb504a12f4d803
Joshua Blum <joshblum@mit.edu>
2014-01-18 18:56:58 -0500

    get pub key refactor

--- truespeak/views.py
+++ truespeak/views.py
@@ -1,18 +1,26 @@
-from django.http import HttpResponse, HttpResponseNotAllowed
-from django import shortcuts
+from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.core.exceptions import ObjectDoesNotExist
-from truespeak.models import *
-from truespeak.common import sendEmailAssociationConfirmation, getNewConfirmationLink, logError, createEmailProfile
-import json, random
+from django import shortcuts
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+from truespeak.common import sendEmailAssociationConfirmation, getNewConfirmationLink, logError, createEmailProfile
+from truespeak.models import *
+
+import random
+import json
 
 
 def redirect(request, page='/home'):
     return shortcuts.redirect(page)
+
+
+def json_response(res):
+    return HttpResponse(json.dumps(res), content_type="application/json")
+
 
 def viewWrapper(view):
     @csrf_exempt
@@ -20,26 +28,33 @@ def viewWrapper(view):
         if not request.user.is_authenticated():
             return HttpResponse("You need to be logged in doe")
         else:
-            return view(request,*args,**kwargs)
+            return view(request, *args, **kwargs)
     return new_view
 
+
 def home(request):
-    return render_to_response('home.html',locals(), context_instance=RequestContext(request))
+    return render_to_response('home.html', locals(), context_instance=RequestContext(request))
+
 
 def goodbye(request):
-    return render_to_response('goodbye.html',locals(), context_instance=RequestContext(request))
+    return render_to_response('goodbye.html', locals(), context_instance=RequestContext(request))
+
 
 def welcome(request, email_address=None):
-    return render_to_response('welcome.html',locals(), context_instance=RequestContext(request))
+    return render_to_response('welcome.html', locals(), context_instance=RequestContext(request))
+
 
 def about(request):
-    return render_to_response('docs.html',locals(), context_instance=RequestContext(request))
+    return render_to_response('docs.html', locals(), context_instance=RequestContext(request))
+
 
 def contact(request):
-    return render_to_response('contact.html',locals(), context_instance=RequestContext(request))
+    return render_to_response('contact.html', locals(), context_instance=RequestContext(request))
+
 
 def initializingPage(request):
-    return render_to_response('initializing.html',locals(), context_instance=RequestContext(request))
+    return render_to_response('initializing.html', locals(), context_instance=RequestContext(request))
+
 
 @ensure_csrf_cookie
 def settingsPage(request):
@@ -47,8 +62,8 @@ def settingsPage(request):
     # if its a post then user is updating some settings
     if request.method == "POST":
         to_return = {
-            "error":None,
-            "message":""
+            "error": None,
+            "message": ""
         }
         if "new_email" in request.POST:
             new_email = request.POST['new_email']
@@ -58,12 +73,13 @@ def settingsPage(request):
             else:
                 to_return['error'] = message
         # return json
-        return HttpResponse(json.dumps(to_return), content_type="application/json")
+        return json_response(to_return)
 
     # otherwise we are just displaying settings
     else:
-        associated_email_addresses = getAssociatedEmailAddresses(user, confirmed=True)
-        return render_to_response('settings.html',locals())
+        associated_email_addresses = getAssociatedEmailAddresses(
+            user, confirmed=True)
+        return render_to_response('settings.html', locals())
 
 
 def confirmEmail(request, link_number):
@@ -73,21 +89,23 @@ def confirmEmail(request, link_number):
     # TODO: check if confirmation link is less than 2 weeks old
     email_profile = email_profile[0]
     user = email_profile.user
-    user.backend = 'django.contrib.auth.backends.ModelBackend' #TODO this is questionable..
+    # TODO this is questionable..
+    user.backend = 'django.contrib.auth.backends.ModelBackend'
     login(request, user)
     if not email_profile.confirmed:
         email_profile.confirmed = True
         email_profile.save()
         return shortcuts.redirect("/initializing/")
     else:
-        logError("second time clicking on confirmation link? " + email_profile.email)
+        logError(
+            "second time clicking on confirmation link? " + email_profile.email)
         return shortcuts.redirect("/settings/")
 
 
 @ensure_csrf_cookie
 def loginPage(request):
     if request.method == "GET":
-        return render_to_response('login.html',locals(), context_instance=RequestContext(request))
+        return render_to_response('login.html', locals(), context_instance=RequestContext(request))
     else:
         email = request.POST['email']
         password = request.POST['password']
@@ -97,10 +115,12 @@ def loginPage(request):
         except ObjectDoesNotExist:
             error = "Oops, this email was not found. <br> Try <a href='/register/'>registering?</a>"
         except MultipleObjectsReturned:
-            logError("multiple email profiles for single email -- this is bad! " + str(email))
+            logError(
+                "multiple email profiles for single email -- this is bad! " + str(email))
         else:
             if not email_profile.confirmed:
-                error = "Oops, this email has not been confirmed. <br> <a href='/reconfirm/" + email + "/'> Resend? </a>"
+                error = "Oops, this email has not been confirmed. <br> <a href='/reconfirm/" + \
+                    email + "/'> Resend? </a>"
             else:
                 user = email_profile.user
                 user = authenticate(username=user.username, password=password)
@@ -112,10 +132,11 @@ def loginPage(request):
                 else:
                     error = "Oops, email/password was not found."
         to_return = {
-            "error":error,
-            "message":"blah"
+            "error": error,
+            "message": "blah"
         }
-        return HttpResponse(json.dumps(to_return), content_type="application/json")
+        return json_response(to_return)
+
 
 def logoutPage(request):
     logout(request)
@@ -125,7 +146,7 @@ def logoutPage(request):
 @ensure_csrf_cookie
 def registerPage(request):
     if request.method == "GET":
-        return render_to_response('register.html',locals(), context_instance=RequestContext(request))
+        return render_to_response('register.html', locals(), context_instance=RequestContext(request))
     else:
         email = request.POST['email']
         password1 = request.POST['password1']
@@ -136,100 +157,46 @@ def registerPage(request):
         elif not password1 == password2:
             error = "Oops, passwords must be the same."
         # check if email already exists
-        already_profile = EmailProfile.objects.filter(email=email, confirmed=True)
+        already_profile = EmailProfile.objects.filter(
+            email=email, confirmed=True)
         if already_profile:
             error = "This email is already associated with an account.<br> Try <a href='/login/'>logging in?</a>"
         already_user = User.objects.filter(username=email)
         if already_user:
             error = "This email is already associated with an account.<br> Try <a href='/login/'>logging in?</a>"
         if not error:
-            user = User.objects.create_user(username=email, email=email, password=password1)
+            user = User.objects.create_user(
+                username=email, email=email, password=password1)
         to_return = {
-            "error":error,
-            "message":"blah"
+            "error": error,
+            "message": "blah"
         }
-        return HttpResponse(json.dumps(to_return), content_type="application/json")
+        return json_response(to_return)
 
 
 def errorView(request, error_dict=None):
     return HttpResponse("My special error view.")
 
 
-# list of identifiers which can be used to find associated public keys
-ALLOWED_IDENTIFIERS = ["email"]
-
 @csrf_exempt
 def getPubKeys(request):
-    """
-        If requested_keys in request.POST:
-            Returns JSON list of pubkey/username data associated with each identifier dict in requested_keys.
-        Else:
-            Returns single pubkey/username data, treating entire request.POST as identifier dict.
-        Each identifier must be a dictionary of identifier:value meant to identify a user.
-        Format of response for each identifier_dict is described under getPubkeysAssociatedWithIndentifiers
-    """
-    if request.method != "POST":
-        return HttpResponseNotAllowed(["POST"])
 
-    requested_keys = request.POST.get('requested_keys')
-    # if post is list of identifier dict
-    if requested_keys:
-        to_return = []
-        for identifier_dict in requested_keys:
-            result = getPubKeysAssociatedWithIndentifiers(identifier_dict)
-            to_return.append(result)
-    # else assuming post is single identifier dict
-    else:
-        to_return = getPubKeysAssociatedWithIndentifiers(request.POST)
+    requested_keys = json.loads(request.GET.get('requested_keys', []))
+    
+    res = {email: getPubKeysAssociatedWithEmail(email) for email in requested_keys}
 
-    # return json
-    return HttpResponse(json.dumps(to_return), content_type="application/json")
+    return json_response(res)
 
 
-def getPubKeysAssociatedWithIndentifiers(identifier_dict):
-    """
-        identifier_dict must be a dictionary of identifier:value meant to identify a user.
-        Returns dictionary:
-         'identifier_used': None if does not exist, string of the identifier that was used if user was found
-         'error': Error message if there was a problem, None if query was successful
-         'pub_keys': None if does not exist, otherwise list of pub_key strings
-         'username': None if does not exist, otherwise parseltongue username associated with pub_keys
-    """
+def getPubKeysAssociatedWithEmail(email):
+    try:
+        email_profile = EmailProfile.objects.get(email=email)
+        user = email_profile.user
+        return getUserPubKeys(user)
+    except Exception:
+        pass
+    return []
 
-    to_return = {}
-    to_return["identifier_used"] = identifier_used = None
-    to_return["error"] = error = None
-    to_return["pub_keys"] = None
-    to_return["username"] = None
-    user = None
-
-    # for all identifier,values to try... give it a go
-    # TODO: should we check if there are conflicting results by different identifiers?
-    for identifier,value in identifier_dict.items():
-
-        if identifier == "email":
-            email = value
-            try:
-                email_profile = EmailProfile.objects.get(email=email)
-                user = email_profile.user
-                identifier_used = identifier
-            except ObjectDoesNotExist:
-                continue
-            except Exception as e:
-                error = e.message
-                break
-        else:
-            error = "invalid identifier"
-
-    to_return["error"] = error # this will still be none if there were no errors
-    if identifier_used: # this means a user was found
-        pub_keys = getUserPubKeys(user)
-        username = user.username
-        to_return["identifier_used"] = identifier_used
-        to_return["pub_keys"] = pub_keys
-        to_return["username"] = username
-
-    return to_return
 
 def uploadPubKey(request):
     """
@@ -239,18 +206,21 @@ def uploadPubKey(request):
     pub_key_text = request.POST['pub_key']
     post_user = request.POST["username"]
     if user.username != post_user:
-        logError("authenticated user is not who they think they are? " + user.username + " " + post_user)
+        logError("authenticated user is not who they think they are? " +
+                 user.username + " " + post_user)
     prior_keys = PubKey.objects.filter(user=user)
     if prior_keys:
         pass
-        # TODO: should send you an email saying a pub_key was uploaded to your account, if not initial registration
-    already = PubKey.objects.filter(user=user,pub_key_text=pub_key_text)
+        # TODO: should send you an email saying a pub_key was uploaded to your
+        # account, if not initial registration
+    already = PubKey.objects.filter(user=user, pub_key_text=pub_key_text)
     if not already:
         pub_key = PubKey(user=request.user, pub_key_text=pub_key_text)
         pub_key.save()
         return HttpResponse("Success")
     else:
         return HttpResponse("Error: Key has already been uploaded.")
+
 
 def uploadPriKey(request):
     """
@@ -259,7 +229,8 @@ def uploadPriKey(request):
     user = request.user
     post_user = request.POST["username"]
     if user.username != post_user:
-        logError("authenticated user is not who they think they are? " + user.username + " " + post_user)
+        logError("authenticated user is not who they think they are? " +
+                 user.username + " " + post_user)
     pri_key_text = request.POST['pri_key']
     already = PriKey.lg.get_or_none(user=user)
     if already:
@@ -270,23 +241,17 @@ def uploadPriKey(request):
         pri_key.save()
     return HttpResponse("Success")
 
+
 def getPriKey(request):
     user = request.user
     pri_key = PriKey.lg.get_or_none(user=user)
     if pri_key:
         to_return = {
-            "success":1,
-            "pri_key":pri_key.pri_key_text
+            "success": 1,
+            "pri_key": pri_key.pri_key_text
         }
     else:
         to_return = {
-            "success":0
+            "success": 0
         }
-    return HttpResponse(json.dumps(to_return), content_type="application/json")
-
-
-
-
-
-
-
+    return json_response(to_return)
