5b2d59c101b0cded09740ab5ccc58ce203d179c2
Joshua Blum <joshblum@mit.edu>
2014-01-27 20:47:42 -0500

    filters

---
+++ truespeak/templatetags/filters.py
@@ -0,0 +1,16 @@
+from django import template
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+register = template.Library()
+
+def fill(template, path):
+    static_path = staticfiles_storage.url(path)
+    return template % static_path
+
+@register.simple_tag
+def include_script(script_name):
+    return fill("""<script type="text/javascript" src="%s.js"></script>""", script_name)
+
+@register.simple_tag
+def include_style(style_name):
+    return fill("""<link type="text/css" rel="stylesheet" href="%s.css" />""", style_name)
