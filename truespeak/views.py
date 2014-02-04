b77a1e7fb889f3e75f95aa018497be7a46389a6c
Joshua Blum <joshblum@mit.edu>
2014-02-03 22:52:57 -0500

    this is good

--- truespeak/views.py
+++ truespeak/views.py
@@ -37,8 +37,6 @@ def view_wrapper(view):
 
 
 def home(request):
-    if request.user.is_authenticated():
-        return shortcuts.redirect("/settings/")
     page_title = "home"
     return render_to_response('home.html', locals(),
                               context_instance=RequestContext(request))
