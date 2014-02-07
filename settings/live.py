b29e1d0afd81803a5e03f41086d3801052276ace
Max Fowler <max_fowler@brown.edu>
2014-02-07 16:40:57 -0500

    s3 for most stuff

--- settings/live.py
+++ settings/live.py
@@ -3,3 +3,5 @@ from settings.common import *
 DATABASES = {
     'default': SECRETS_DICT['DATABASES']['LIVE']
 }
+
+STATIC_URL = "https://s3.amazonaws.com/parseltongue/"
--- truespeak/templatetags/filters.py
+++ truespeak/templatetags/filters.py
@@ -9,8 +9,10 @@ def fill(template, path):
 
 @register.simple_tag
 def include_script(script_name):
-    return fill("""<script type="text/javascript" src="%s.js"></script>""", script_name)
+    src = script_name + ".js"
+    return fill("""<script type="text/javascript" src="%s"></script>""", src)
 
 @register.simple_tag
 def include_style(style_name):
-    return fill("""<link type="text/css" rel="stylesheet" href="%s.css" />""", style_name)
+    href = style_name + ".css"
+    return fill("""<link type="text/css" rel="stylesheet" href="%s" />""", href)
